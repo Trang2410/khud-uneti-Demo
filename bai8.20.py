a904da85f8bc32e27b3d4fde42653562461931f1
thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>
2022-11-30 14:32:15 +0700

    Add files via upload
    Signed-off-by: thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>

---
+++ bai8.20.py
@@ -0,0 +1,11 @@
+import math
+x=float(input("Nhập vào giá trị của x="))
+Ex=1
+n=1
+t=x
+while math.fabs(t)>=0.0001: #(10 mũ - 4)
+    Ex+=t
+    n+=1
+    # t*=x/(float(n))
+    t=t*(x/(float(n)))
+print("Giá trị gần đúng của e mũ x là %0.6f"%Ex)
