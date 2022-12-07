4d7359838593cdfa71965d4a1a08155abc1c97c8
thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>
2022-12-07 10:42:52 +0700

    Add files via upload
    Signed-off-by: thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>

---
+++ bai9.4.py
@@ -0,0 +1,11 @@
+def  Tinh_gia_tri ( n , x ):
+    return ( x * x + 1 ) ** n
+print ( 'Nhập vào x:' )
+x = int (input())
+print ( 'Nhập vò n:' )
+n = int (input())  
+S = Tinh_gia_tri ( n , x )
+int ( 'S=' , S )
+    
+        
+
