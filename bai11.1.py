cd48a474a48c56270fb1317914bfb236ba92f501
thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>
2022-12-08 20:25:06 +0700

    Add files via upload
    Signed-off-by: thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>

---
+++ bai11.1.py
@@ -0,0 +1,9 @@
+a = [1,2,3]
+print(len(a))
+b = [1,[2,3]]
+print(len(b))
+c = []
+print(len(c))
+d = [1,2,3][1:]
+print(len(d))
+
---
+++ bai11.3.py
@@ -0,0 +1,3 @@
+a = ["ant","bear","cat","dog","elephant","fish","goat","hippo"]
+print("Số lượng thú cần tìm là:", len(a))
+print("Con thú cần tìm là:", a[7])
