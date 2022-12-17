e20493c2408d3b0d61aa718c1b12c471b6d1a5e2
thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>
2022-12-18 02:37:19 +0700

    Add files via upload
    Signed-off-by: thutrang0028 <117258475+tranthithutrang0028@users.noreply.github.com>

--- bai11.12.py
+++ bai11.12.py
@@ -1,8 +1,10 @@
-a=(1,3,4,2)
-b=(8,6,5,7)
-c=a,b
+a=[1,3,4,2]
+b=[8,6,5,7]
+c=a+b
 print("Tuple c là:",c)
+d=[]
 d=a + b
+d.sort()
 print("Tuple d là:",d)
 print("Phần tử thứ 3 của d là:",d[2])
 print("3 phần tử cuối cùng của d là:",d[5:8])
