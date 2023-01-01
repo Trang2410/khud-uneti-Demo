926ccd08212744d499f39377e393e71a37a07ee1
Tranthithutrang0028DHKL16A2 <117258475+tranthithutrang0028@users.noreply.github.com>
2023-01-01 18:21:58 +0700

    Add files via upload
    Signed-off-by: Tranthithutrang0028DHKL16A2 <117258475+tranthithutrang0028@users.noreply.github.com>

---
+++ bai11.2.py
@@ -0,0 +1,6 @@
+doi1=["Steven","Neena","Lex","Alexander","Bruce"]
+doi2=["David","Jack","Bill","Tom","Mike","Daniel"]
+doi3=["Payam","Kevin","Sigal","Mike","Alexander","Adam"]
+print("Đội trưởng tồi tệ nhất đội 1 là:",doi1[1] )
+print("Đội trưởng tồi tệ nhất đội 2 là:",doi2[2] )
+print("Đội trưởng tồi tệ nhất đội 3 là:",doi3[3] )
