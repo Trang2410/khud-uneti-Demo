8140c73c4d4deee1b6a245cae2be8391e6511574
Tranthithutrang0028DHKL16A2 <117258475+tranthithutrang0028@users.noreply.github.com>
2023-01-01 18:23:01 +0700

    Add files via upload
    Signed-off-by: Tranthithutrang0028DHKL16A2 <117258475+tranthithutrang0028@users.noreply.github.com>

---
+++ bai11.11.py
@@ -0,0 +1,10 @@
+#Chương trình thực hiện việc xử lý tuple
+#Tạo tuple
+tuple=("red","green","yellow","blue","black","while","pink","orange","red","blue")
+print("Tuple là:",tuple)
+n=int(input(tuple[0:9]))
+print("Nhập số từ 0 đến 9:",n)
+m=int(input(tuple[-1:-9]))
+print("Nhập số từ -1 đến -9:",m)
+
+
